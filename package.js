bcc7e3de55481335aae51fa541e6dbc855ccc9c2
lvdo <laurens@q42.nl>
2016-01-30 21:17:29 +0100

    Added dependencies

--- package.js
+++ package.js
@@ -7,7 +7,11 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+  // The package probably works with older versions
   api.versionsFrom('1.2.1');
+  api.use('react@0.14.3');
+  api.use('reactive-var@1.0.6');
+
   api.export('AutoVarMixin');
   api.addFiles('autovars.js');
 });
