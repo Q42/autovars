a0a467679730c22cb8adf9bba74e5e6bea45ceb7
Lukas van Driel <lukas@3l.nl>
2016-03-09 14:48:14 -0800

    No specific versions
    the constraint isn't satisfied by the new 1.3-beta.16 meteor version

--- package.js
+++ package.js
@@ -9,8 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   // The package probably works with older versions
   api.versionsFrom('1.2.1');
-  api.use('react@0.14.3');
-  api.use('reactive-var@1.0.6');
+  api.use(['react','reactive-var']);
 
   api.export('AutoVarMixin');
   api.addFiles('autovars.js');
